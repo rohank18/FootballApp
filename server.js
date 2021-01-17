34a74e41f827eb9bb943d4c43e9432cc935c091f
Rohan <rohankhunteta3000@gmail.com>
2021-01-17 18:33:36 +0530

    Setup heroku port

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
@@ -84,6 +85,6 @@ app.get('/teams', (req, res) => {
     })  
 })
 
-app.listen(3000, () => {
-    console.log('Server is up and running on Port 3000');
+app.listen(port, () => {
+    console.log(`Server is up and running on Port ${port}`);
 });
